d32b16021e8dcb63863d998f558007a8740fccaa
Ed Elliot <Ed.Elliot@company.com>
2017-10-02 14:27:07 +0100

    query to validate class exists

--- CLR/tSQLt.Client.Net/SerizlizableObjects/Queries.cs
+++ CLR/tSQLt.Client.Net/SerizlizableObjects/Queries.cs
@@ -29,6 +29,17 @@ namespace tSQLt.Client.Net
 
             return String.Format("exec tSQLt.RunTestClass '{0}'", testClass);
         }
+        public static string GetQueryForValidateClass(string testClass)
+        {
+            /*declare @schema_name nvarchar(255) = 'abc1'
+
+declare @schema_count int = (select count(*) from sys.schemas where name = @schema_name);
+declare @test_class_count int = (select count(*) from sys.extended_properties ep join sys.schemas s on ep.major_id = s.schema_id
+	where ep.class_desc = 'SCHEMA' and ep.name = 'tSQLt.TestClass' and s.name = @schema_name);
+
+select @schema_count schema_count, @test_class_count test_class_count
+*/
+        }
 
         private static string QuoteName(string name)
         {
---
+++ CLR/tSQLt.Client.Net/TestValidators/TestClassValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using tSQLt.Client.Net.Gateways;
+
+namespace tSQLt.Client.Net.TestValidators
+{
+    abstract class TestValidator{
+        public TestValidator(ISqlServerGateway gateway);
+    }
+
+}
+    class TestClassValidator
+    {
+
+    }
+}
